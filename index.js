1e064bad919da5006ebb9da27b00458893c6a54a
moonraker22 <zxkst2@protonmail.com>
2022-04-29 20:45:14 -0500

    Exercises 3.15.-3.18 Done

--- index.js
+++ index.js
@@ -1,42 +1,29 @@
 require("dotenv").config();
 const express = require("express");
-const morgan = require("morgan");
-const path = require("path");
-const fs = require("fs");
-const { log } = require("console");
 const cors = require("cors");
 const Person = require("./models/person");
+const logger = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
+
+app.use(express.static("build"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use(express.static("build"));
-
-const logger = morgan((tokens, req, res) => {
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, "content-length"),
-    "-",
-    tokens["response-time"](req, res),
-    "ms",
-    "\n",
-    `Body: ${JSON.stringify(req.body)}`,
-  ].join(" ");
-});
 app.use(logger);
+app.use(errorHandler);
 
 // Get the phonebook
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", (request, response, next) => {
   Person.find({})
     .then((result) => {
       response.json(result);
     })
-    .catch((err) => {
-      console.log(err);
+    .catch((error) => {
+      console.log(error.message);
+      next(error);
     });
 });
 
@@ -57,7 +44,7 @@ app.get("/info", (request, response) => {
 });
 
 // Get a single person
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   Person.findById(request.params.id)
     .then((result) => {
       if (result) {
@@ -66,14 +53,18 @@ app.get("/api/persons/:id", (request, response) => {
         response.status(404).send({ message: "Person not found" });
       }
     })
-    .catch((err) => {
-      console.log(err);
-      response.status(400).send({ error: "malformatted id" });
+    // .catch((err) => {
+    //   console.log(err);
+    //   response.status(400).send({ error: "malformatted id" });
+    // });
+    .catch((error) => {
+      console.log(error.message);
+      next(error);
     });
 });
 
 // Delete a single person
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then((result) => {
       if (result) {
@@ -82,13 +73,17 @@ app.delete("/api/persons/:id", (request, response) => {
         response.status(404).send({ message: "Person not found" });
       }
     })
-    .catch((err) => {
-      console.log(err);
+    // .catch((err) => {
+    //   console.log(err);
+    // });
+    .catch((error) => {
+      console.log(error.message);
+      next(error);
     });
 });
 
 // Create a new person in the phonebook
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const { name, number } = request.body;
   if (!name) {
     return response.status(400).json({ error: "name missing" });
@@ -106,9 +101,39 @@ app.post("/api/persons", (request, response) => {
       })
       .catch((err) => {
         console.log(err);
+        next(err);
       });
   }
 });
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body;
+  if (!name) {
+    return response.status(400).json({ error: "name missing" });
+  } else if (!number) {
+    return response.status(400).json({ error: "number missing" });
+  } else {
+    Person.findByIdAndUpdate(request.params.id, { name, number })
+      .then((updatedPerson) => {
+        if (updatedPerson) {
+          response.json(updatedPerson);
+        } else {
+          response.status(404).send({ message: "Person not found" });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        next(err);
+      });
+  }
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
---
+++ middleware/logger.js
@@ -0,0 +1,17 @@
+const morgan = require("morgan");
+
+const logger = morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, "content-length"),
+    "-",
+    tokens["response-time"](req, res),
+    "ms",
+    "\n",
+    `Body: ${JSON.stringify(req.body)}`,
+  ].join(" ");
+});
+
+module.exports = logger;
